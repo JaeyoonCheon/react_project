d6ddb0430303d957d7998111d2308a874908ed1e
JaeyoonCheon <cjswodbs50@gmail.com>
2022-01-05 16:21:56 +0900

    HTML/CSS/JS example static/dynamic html

--- primer/src/App.js
+++ primer/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
+const count = 4;
 export default class App extends Component {
   render = () => (
     <h4 className="bg-primary text-white text-center p-2 m-1">
-      Component Element
+      Number of things : {count % 2 === 0 ? "Even" : "Odd"}
     </h4>
   );
 }
