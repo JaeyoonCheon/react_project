e4a82714e147e57287c50342fbb36c122ca514de
JaeyoonCheon <cjswodbs50@gmail.com>
2022-01-05 16:46:23 +0900

    HTML/CSS/JS example bootstrap grid layout

--- primer/src/App.js
+++ primer/src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
-
-const count = 4;
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4,
+    };
+  }
+
+  isEven(value) {
+    return value % 2 === 0 ? "Even" : "Odd";
+  }
+
+  getClassName(value) {
+    return value % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1";
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1 });
+
   render = () => (
-    <h4 className="bg-primary text-white text-center p-2 m-1">
-      Number of things : {count % 2 === 0 ? "Even" : "Odd"}
-    </h4>
+    <div className="container-fluid p-4">
+      <div className="row bg-info text-white p-2">
+        <div className="col font-weight-bold">Value</div>
+        <div className="col-6 font-weight-bold">Even?</div>
+      </div>
+      <div className="row bg-light p-2 border">
+        <div className="col">{this.state.count}</div>
+        <div className="col-6">{this.isEven(this.state.count)}</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-info m-2" onClick={this.handleClick}>
+            Click me
+          </button>
+          Number of things : {this.isEven(this.state.count)}
+        </div>
+      </div>
+    </div>
   );
 }
