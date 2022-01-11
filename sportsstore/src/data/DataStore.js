0b696281d0cb6fb22c7607d6f77976ca37365d35
JaeyoonCheon <cjswodbs50@gmail.com>
2022-01-12 00:23:18 +0900

    sport shop REST api Axios

--- sportsstore/src/data/DataStore.js
+++ sportsstore/src/data/DataStore.js
@@ -2,7 +2,10 @@ import { createStore } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { applyMiddleware } from "redux";
+import { asyncActions } from "./AsyncMiddleware";
 
 export const SportStoreDataStore = createStore(
-  CommonReducer(ShopReducer, CartReducer)
+  CommonReducer(ShopReducer, CartReducer),
+  applyMiddleware(asyncActions)
 );
